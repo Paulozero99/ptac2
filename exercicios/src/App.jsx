8e47f70fd7a4ec24633d77a1caf0339650e12c0c
Paulozero99 <opaulohenrique363@gmail.com>
2026-09-02 15:13:54 -0400

    Criado o minimo nescessario para chamar o App.jsx em um componente

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
 export default function App() {
+    //lógica de programação
     return (
+        //rederização na viewport
         <>
-            
+            <p>teste</p>
         </>
     )
 }
